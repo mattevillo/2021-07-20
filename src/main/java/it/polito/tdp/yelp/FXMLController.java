1b90a7924eef0e1f925401915386a5b853006fa5
Matteo Villosio <s258803@studenti.polito.it>
2022-05-26 11:54:46 +0200

    Yelp - Grafo

--- src/main/java/it/polito/tdp/yelp/FXMLController.java
+++ src/main/java/it/polito/tdp/yelp/FXMLController.java
@@ -5,9 +5,11 @@
 package it.polito.tdp.yelp;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import it.polito.tdp.yelp.model.Model;
+import it.polito.tdp.yelp.model.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -38,13 +40,13 @@ public class FXMLController {
     private TextField txtX2; // Value injected by FXMLLoader
 
     @FXML // fx:id="cmbAnno"
-    private ComboBox<?> cmbAnno; // Value injected by FXMLLoader
+    private ComboBox<Integer> cmbAnno; // Value injected by FXMLLoader
 
     @FXML // fx:id="txtN"
     private TextField txtN; // Value injected by FXMLLoader
 
     @FXML // fx:id="cmbUtente"
-    private ComboBox<?> cmbUtente; // Value injected by FXMLLoader
+    private ComboBox<User> cmbUtente; // Value injected by FXMLLoader
 
     @FXML // fx:id="txtX1"
     private TextField txtX1; // Value injected by FXMLLoader
@@ -54,11 +56,36 @@ public class FXMLController {
 
     @FXML
     void doCreaGrafo(ActionEvent event) {
+    	
+    	String minReviewS = txtN.getText();
+    	try {
+    	int minReview = Integer.parseInt(minReviewS);
+    	
+    	Integer anno = cmbAnno.getValue();
+    		if (anno==null) {
+    			txtResult.setText("Devi selezionare un anno valido");
+    			return;
+    		}
+    		
+    		String msg = model.creaGrafo(minReview, anno);
+    		txtResult.setText(msg);
+    		cmbUtente.getItems().clear();
+    		cmbUtente.getItems().addAll(model.getUsers());
+    		
+    	} catch (NumberFormatException e) {
+    		txtResult.setText("Devi inserire un numero valido");
+    		return;
+    	}
+    	
 
     }
 
     @FXML
     void doUtenteSimile(ActionEvent event) {
+    	
+    	User u = cmbUtente.getValue();
+    	List<User> vicini = model.utentiPiuSimili(u);
+    	txtResult.setText(vicini.toString());
 
     }
     
@@ -79,6 +106,11 @@ public class FXMLController {
         assert cmbUtente != null : "fx:id=\"cmbUtente\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtX1 != null : "fx:id=\"txtX1\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+        
+        for(int i=2005; i<=2013; i++) {
+        	cmbAnno.getItems().add(i);
+        }
+        
 
     }
     
--- src/main/java/it/polito/tdp/yelp/db/YelpDao.java
+++ src/main/java/it/polito/tdp/yelp/db/YelpDao.java
@@ -111,5 +111,79 @@ public class YelpDao {
 		}
 	}
 	
+	public List<User> getUsersWithReviews(int minReviews){
+		String sql = "SELECT u.user_id, u.votes_funny, u.votes_useful, u.votes_cool, u.name, u.average_stars,  review_count "
+				+ "FROM Users u, Reviews r "
+				+ "WHERE u.user_id = r.user_id "
+				+ "GROUP BY u.user_id, u.votes_funny, u.votes_useful, u.votes_cool, u.name, u.average_stars,  review_count "
+				+ "HAVING COUNT(*)>=? ";
+
+		List<User> result = new ArrayList<User>();
+		Connection conn = DBConnect.getConnection();
+
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, minReviews);
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+
+				User user = new User(res.getString("user_id"),
+						res.getInt("votes_funny"),
+						res.getInt("votes_useful"),
+						res.getInt("votes_cool"),
+						res.getString("name"),
+						res.getDouble("average_stars"),
+						res.getInt("review_count"));
+				
+				result.add(user);
+			}
+			res.close();
+			st.close();
+			conn.close();
+			return result;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 	
+	
+	
+	public int calcolaSimilarita(User u1, User u2, int anno) {
+		
+		String sql="SELECT COUNT(*) as similarita "
+				+ "FROM Reviews r1, Reviews r2 "
+				+ "WHERE r1.business_id = r2.business_id "
+				+ "AND r1.user_id = ? "
+				+ "AND r2.user_id = ? "
+				+ "AND YEAR(r1.review_date) = ? "
+				+ "AND YEAR(r2.review_date) = ? ";
+		
+		Connection conn = DBConnect.getConnection();
+		
+		
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setString(1,u1.getUserId());
+			st.setString(2, u2.getUserId());
+			st.setInt(3, anno);
+			st.setInt(4, anno);
+			
+			ResultSet res = st.executeQuery();
+			
+			res.first();
+			int similarita = res.getInt("similarita");
+			
+			res.close();
+			st.close();
+			conn.close();
+			return similarita;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return -1;
+		}
+	
+}
 }
